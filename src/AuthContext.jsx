9e1d10f93d100f0047e878c61305e3ebffaa6924
Sherin <sherin.daniel89@yahoo.com>
2025-09-28 19:25:33 -0400

    Auth update

--- src/AuthContext.jsx
+++ src/AuthContext.jsx
@@ -1,18 +1,42 @@
 import { createContext, useContext, useState } from "react";
-
 const API = "https://fsa-jwt-practice.herokuapp.com";
-
 const AuthContext = createContext();
 
 export function AuthProvider({ children }) {
   const [token, setToken] = useState();
   const [location, setLocation] = useState("GATE");
 
-  // TODO: signup
+  // Sending credentials to receive a token //
+  const signup = async (credentials) => {
+    try {
+      const response = await fetch(API + "/signup", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(credentials),
+      });
+      const result = await response.json();
+      setToken(result.token);
+      setLocation("TABLET");
+    } catch (e) {
+      console.error(e);
+    }
+  };
 
-  // TODO: authenticate
+  // Sending tokens to Authenticate the user //
+  const authenticate = async () => {
+    try {
+      if (!token) throw Error("No token found.");
+      const response = await fetch(API + "/authenticate", {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      if (!response.ok) throw Error("Authentication failed.");
+      setLocation("TUNNEL");
+    } catch (e) {
+      console.error(e);
+    }
+  };
 
-  const value = { location };
+  const value = { signup, authenticate, location };
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
 
